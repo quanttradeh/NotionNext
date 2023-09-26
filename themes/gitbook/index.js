fb96edb8fd3ff3f68336c4b1d63635585fcae015
quanttradeh <145670959+quanttradeh@users.noreply.github.com>
2023-09-26 13:03:19 +0800

    Update index.js

--- themes/gitbook/index.js
+++ themes/gitbook/index.js
@@ -220,7 +220,7 @@ const LayoutSlug = (props) => {
             {!lock && <div id='container'>
 
                 {/* title */}
-                <h1 className="text-3xl pt-12  dark:text-gray-300">{post?.title}</h1>
+                <h1 className="text-3xl font-bold pt-12  dark:text-gray-300">{post?.title}</h1>
 
                 {/* Notion文章主体 */}
                 {post && (<section id="article-wrapper" className="px-1">
