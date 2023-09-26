97ebbc6caa2f27fe65d2ddffe259cc0c3fa2b2fe
quanttradeh <145670959+quanttradeh@users.noreply.github.com>
2023-09-26 12:51:06 +0800

    Update Footer.js

--- themes/gitbook/components/Footer.js
+++ themes/gitbook/components/Footer.js
@@ -18,7 +18,7 @@ const Footer = ({ siteInfo }) => {
             <hr className='pb-2' />
 
             <div className='flex justify-center'>
-                <div><i className='mx-1 animate-pulse fas fa-heart' /> <a href={BLOG.LINK} className='underline font-bold text-gray-500 dark:text-gray-300 '>{BLOG.AUTHOR}</a> <br /></div>
+                <div><i className='mx-1 animate-pulse fas fa-heart' /> <a href={BLOG.LINK} className='underline font-bold text-gray-500 dark:text-gray-300 '>{BLOG.AUTHOR}</a>&nbsp;&nbsp; <br /></div>
                 © {`${copyrightDate}`}
             </div>
             <span className='hidden busuanzi_container_site_pv'>
